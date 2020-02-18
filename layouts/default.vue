1a14593245a7055ea3e46f910ff47b063fdd8cb2
jonathantullis <jon.tullis558@gmail.com>
2020-02-18 11:59:19 -0700

    Added carousel and biography components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,28 @@
 <template>
   <v-app dark>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="appTitle" class="font-weight-bold title" />
+      <span class="mx-3 mb-1" style="transform: scale(0.3, 3);"> | </span>
+      <v-toolbar-title v-text="pageTitle" class="subtitle-1" />
+      <v-spacer />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
-      :clipped="clipped"
       fixed
       app
+      right
     >
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
+          @click="pageTitle = item.title"
           router
           exact
         >
@@ -24,64 +35,17 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
     <v-content>
-      <v-container>
+      <v-container
+        fluid
+        class="pa-0"
+      >
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :fixed="fixed"
       app
+      :fixed="true"
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -92,25 +56,23 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          title: 'Listen',
           to: '/inspire'
         }
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      appTitle: 'BROKEN ACCORD',
+      pageTitle: 'Home'
+
     }
   }
 }
