13250311317b4395c6bbbc4664e19574618b5ec4
Jonny Tullis <jon.tullis558@gmail.com>
2021-02-02 16:04:04 -0700

    Updated padding in social links

--- components/social.vue
+++ components/social.vue
@@ -2,33 +2,33 @@
   <v-container>
     <h1 class="mb-2">Follow</h1>
     <h3>Keep up with the latest news and upcoming shows</h3>
-    <v-row class="my-4">
+    <v-row align="center" style="height: 100%;">
       <v-col
         v-for="service in services"
         :key="service.name"
+        style="text-align: center;"
+        class="my-5"
         cols="6"
         sm="3"
         md="3"
         lg="3"
         xl="3"
       >
-        <v-row style="height: 100%;" align="center" justify="center">
-          <v-btn
-            color="transparent"
-            height="80"
-            :width="service.width"
+        <v-btn
+          color="transparent"
+          depressed
+          :width="Number(service.width) + 8"
+          :height="Number(service.height) + 8"
+          :style="`padding: 8px; border-radius:${service.radius}`"
+          :href="service.url"
+          target="_blank"
+        >
+          <img
+            :src="service.src"
             :height="service.height"
-            :style="`padding: 8px; border-radius:${service.radius}`"
-            elevation="6"
-            :href="service.url"
-          >
-            <img
-              :src="service.src"
-              :height="service.height"
-              :alt="service.name"
-            />
-          </v-btn>
-        </v-row>
+            :alt="service.name"
+          />
+        </v-btn>
       </v-col>
     </v-row>
     <h3 class="font-weight-bold mb-2">Follow, Like, Comment</h3>
@@ -45,14 +45,14 @@
           {
             src: require('static/logos/tiktok/tiktok.png'),
             url: 'https://www.tiktok.com/@brokenaccord',
-            radius: '50%',
-            width: '85',
-            height: '85'
+            radius: '10px',
+            width: '80',
+            height: '80'
           },
           {
             src: require('static/logos/instagram/IG_Glyph_Fill.png'),
             url: 'https://www.instagram.com/brokenaccord.band/',
-            radius: '22px',
+            radius: '24px',
             width: '80',
             height: '80'
           },
@@ -61,7 +61,7 @@
             url: 'https://www.youtube.com/channel/UCYx4KXiUDp7zutd1nruIm2w?view_as=subscriber',
             radius: '18px',
             width: '99',
-            height: '70'
+            height: '65'
           },
           {
             src: require('static/logos/facebook/f_logo_RGB-Blue_250.png'),
